ca625aa186a055c60bd3b5a508a8852861424859
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-05-18 13:04:00 +0300

    Refactor: update brain-progression-logic.js
    Improved constant naming, moved nested functions
    to outer scope, improved progression generation.

--- src/games/brain-progression-logic.js
+++ src/games/brain-progression-logic.js
@@ -1,55 +1,43 @@
-import { getRandomInt, getRandomOperator } from '../math.js';
+import getRandomInt from '../math.js';
 
 const gameTask = 'What number is missing in the progression?';
 
-const runGameProgression = () => {
+const getRandomStep = () => {
+  const randomInt = getRandomInt(2);
+  switch (randomInt) {
+    case 0:
+      return 1;
+    case 1:
+      return 3;
+    default:
+      return 5;
+  }
+};
+
+const getRandomProgression = () => {
   const randomInt = getRandomInt();
-  const randomOperator = getRandomOperator();
+  const progressionStep = getRandomStep();
+  const hiddenIndex = getRandomInt(9);
+  const progression = [];
 
-  const getRandomStep = (operator) => {
-    switch (operator) {
-      case '+':
-        return 1;
-      case '-':
-        return 3;
-      default:
-        return 5;
-    }
-  };
+  for (let i = randomInt; progression.length < 10; i += progressionStep) {
+    progression.push(i);
+  }
 
-  const randomStep = getRandomStep(randomOperator);
-  const randomIndex = Math.round(Math.random() * (9 - 0) + 0);
+  const rightAnswer = progression[hiddenIndex];
+  progression[hiddenIndex] = '..';
+  const progressionWithHiddenElement = progression.join(' ');
+  const result = [rightAnswer, progressionWithHiddenElement];
 
-  const getRandomProgression = () => {
-    const result = [];
-    for (let i = randomInt; result.length < 10; i += randomStep) {
-      result.push(i);
-    }
-    return result;
-  };
+  return result;
+};
 
-  const randomProgression = getRandomProgression();
-
-  const hideRandomElement = () => {
-    const coll = [];
-    for (let i = 0; i < randomProgression.length; i += 1) {
-      if (i === randomIndex) {
-        coll.push('..');
-      } else {
-        coll.push(randomProgression[i]);
-      }
-    }
-
-    return coll.join(' ');
-  };
-
-  const progressionWithHiddenElement = hideRandomElement(randomProgression);
+const runBrainProgression = () => {
+  const [rightAnswer, progressionWithHiddenElement] = getRandomProgression();
   const gameQuestion = `${progressionWithHiddenElement}`;
-  const rightAnswer = randomProgression[randomIndex];
-
   const result = [gameQuestion, `${rightAnswer}`];
 
   return result;
 };
 
-export { gameTask, runGameProgression };
+export { gameTask, runBrainProgression };
