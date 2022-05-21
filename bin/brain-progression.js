ea75785f916cee8293a7e653b1338e7a8d006266
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-05-21 10:17:53 +0300

    Refactor: update naming

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { gameTask, runBrainProgression } from '../src/games/brain-progression.js';
+import { brainProgressionTask, generateBrainProgression } from '../src/games/brain-progression.js';
 import engine from '../src/engine.js';
 
-engine(gameTask, runBrainProgression);
+engine(brainProgressionTask, generateBrainProgression);
--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,6 +1,6 @@
 import getRandomInt from '../math.js';
 
-const gameTask = 'What number is missing in the progression?';
+const brainProgressionTask = 'What number is missing in the progression?';
 
 const getRandomStep = () => {
   const randomInt = getRandomInt(2);
@@ -14,22 +14,22 @@ const getRandomStep = () => {
   }
 };
 
-const runBrainProgression = () => {
-  const randomInt = getRandomInt();
+const generateBrainProgression = () => {
+  const progressionStart = getRandomInt();
   const progressionStep = getRandomStep();
-  const hiddenIndex = getRandomInt(9);
+  const indexOfHiddenStep = getRandomInt(9);
   const progression = [];
 
-  for (let i = randomInt; progression.length < 10; i += progressionStep) {
+  for (let i = progressionStart; progression.length < 10; i += progressionStep) {
     progression.push(i);
   }
 
-  const rightAnswer = progression[hiddenIndex];
-  progression[hiddenIndex] = '..';
+  const rightAnswer = progression[indexOfHiddenStep];
+  progression[indexOfHiddenStep] = '..';
   const gameQuestion = `${progression.join(' ')}`;
   const result = [gameQuestion, `${rightAnswer}`];
 
   return result;
 };
 
-export { gameTask, runBrainProgression };
+export { brainProgressionTask, generateBrainProgression };
