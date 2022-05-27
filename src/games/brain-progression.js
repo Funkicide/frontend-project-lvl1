9a04574eaee499c95a6e42bcc23b57a352280a16
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-05-27 15:31:59 +0300

    Refactor: update brainProgression
    Changed step generation from function with
    three variants to more simple and varied solution.
    Also moved right answer to string conversion from
    result to constant init.

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -2,32 +2,20 @@ import getRandomInt from '../math.js';
 
 const brainProgressionTask = 'What number is missing in the progression?';
 
-const getRandomStep = () => {
-  const randomInt = getRandomInt(2);
-  switch (randomInt) {
-    case 0:
-      return 1;
-    case 1:
-      return 3;
-    default:
-      return 5;
-  }
-};
-
 const generateBrainProgression = () => {
   const progressionStart = getRandomInt();
-  const progressionStep = getRandomStep();
-  const indexOfHiddenStep = getRandomInt(9);
+  const progressionStep = getRandomInt(1, 10);
+  const indexOfHiddenStep = getRandomInt(0, 9);
   const progression = [];
 
   for (let i = progressionStart; progression.length < 10; i += progressionStep) {
     progression.push(i);
   }
 
-  const rightAnswer = progression[indexOfHiddenStep];
+  const rightAnswer = `${progression[indexOfHiddenStep]}`;
   progression[indexOfHiddenStep] = '..';
   const gameQuestion = `${progression.join(' ')}`;
-  const result = [gameQuestion, `${rightAnswer}`];
+  const result = [gameQuestion, rightAnswer];
 
   return result;
 };
