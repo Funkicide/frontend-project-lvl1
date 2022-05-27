f70f42515b178d9abfca2e3d840fa58eb6307cf1
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-05-27 16:09:43 +0300

    Refactor: update brainCalc
    Changed method of storage of answers
    to object because it's better this way.
    Also moved toString coversion from export
    to const init inself.

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -2,17 +2,23 @@ import getRandomInt from '../math.js';
 
 const brainCalcTask = 'What is the result of the expression?';
 
+const operators = ['+', '-', '*'];
+
 const generateBrainCalc = () => {
   const firstInt = getRandomInt();
   const secondInt = getRandomInt();
-  const indexOfExpression = getRandomInt(2);
+  const operator = operators[getRandomInt(0, 2)];
 
-  const gameQuestions = [`${firstInt} + ${secondInt}`, `${firstInt} - ${secondInt}`, `${firstInt} * ${secondInt}`];
-  const rightAnswers = [firstInt + secondInt, firstInt - secondInt, firstInt * secondInt];
-  const gameQuestion = gameQuestions[indexOfExpression];
-  const rightAnswer = rightAnswers[indexOfExpression];
+  const operations = {
+    '+': firstInt + secondInt,
+    '-': firstInt - secondInt,
+    '*': firstInt * secondInt,
+  };
 
-  const result = [gameQuestion, `${rightAnswer}`];
+  const gameQuestion = `${firstInt} ${operator} ${secondInt}`;
+  const rightAnswer = `${operations[operator]}`;
+
+  const result = [gameQuestion, rightAnswer];
 
   return result;
 };
