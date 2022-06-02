46dd9d187161e101f1136f5d7d9d8902ed29811e
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-06-02 21:39:18 +0300

    Refactor: update brain-calc.js
    Changed operations object values to functions.

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -2,21 +2,22 @@ import getRandomInt from '../math.js';
 
 const brainCalcTask = 'What is the result of the expression?';
 
+const operations = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+};
+
 const generateBrainCalc = () => {
   const firstInt = getRandomInt();
   const secondInt = getRandomInt();
 
-  const operations = {
-    '+': firstInt + secondInt,
-    '-': firstInt - secondInt,
-    '*': firstInt * secondInt,
-  };
-
   const operators = Object.keys(operations);
   const operator = operators[getRandomInt(0, 2)];
+  const operation = operations[operator](firstInt, secondInt);
 
   const gameQuestion = `${firstInt} ${operator} ${secondInt}`;
-  const rightAnswer = `${operations[operator]}`;
+  const rightAnswer = `${operation}`;
 
   const result = [gameQuestion, rightAnswer];
 
