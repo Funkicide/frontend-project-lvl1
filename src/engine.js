f7aede678ab8f18063318d41cfc855578091ebcf
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-06-02 18:20:39 +0300

    Refactor: reuse greetUser func in engine.js

--- src/engine.js
+++ src/engine.js
@@ -1,11 +1,10 @@
 import readlineSync from 'readline-sync';
+import greetUser from './cli.js';
 
 const maxRoundCount = 3;
 
 const engine = (gameTask, generateGame) => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  const userName = greetUser();
 
   console.log(gameTask);
 
