cf0eb7138fa4e31839b9840b5b3d43bb73034f69
Funkicide <104210070+Funkicide@users.noreply.github.com>
2022-06-05 13:53:18 +0300

    Refactor: rename function parameters

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,7 +3,7 @@ import getRandomInt from '../utils.js';
 
 const task = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (int) => int % 2 === 0;
+const isEven = (number) => number % 2 === 0;
 
 const generateRound = () => {
   const randomInteger = getRandomInt();
